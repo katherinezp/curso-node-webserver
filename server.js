a310ae16042ba22ddb416b0184ce65a5f0717abf
Zurita Poblete <katherine.zurita@cencosud.cl>
2018-10-19 16:56:13 -0300

    se agrega projects

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.listen(puerto, () => {
     console.log(`servidor listo en puerto ${puerto}.`);
 });
